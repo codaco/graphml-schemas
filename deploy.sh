ff2e7350dd33b872eb24bbd01fce7e5995bacdc7
Joshua Melville <joshua.melville@northwestern.edu>
2020-02-05 19:43:53 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 set -o nounset
 set -o errexit
 
-DEPLOY_SOURCE_DIR="_site/"
-DEPLOY_DEST_DIR="/var/www/networkcanvas.com"
+DEPLOY_SOURCE_DIR="xmlns/"
+DEPLOY_DEST_DIR="/var/www/schema.networkcanvas.com/xmlns"
 DEPLOY_SERVER="networkcanvas.com"
 DEPLOY_ACCOUNT=`whoami`
 
@@ -25,9 +25,6 @@ done
 # Set the environment by loading from the file "environment" in the same directory
 DIR="$( cd "$( dirname $( dirname "$0" ) )" && pwd)"
 [ -f "$DIR/.env" ] && source "$DIR/.env"
-
-echo "Building production Jekyll site"
-JEKYLL_ENV=production bundle exec jekyll build
 
 echo "Deploying ${DIR}/${DEPLOY_SOURCE_DIR} to ${DEPLOY_ACCOUNT}@${DEPLOY_SERVER}:${DEPLOY_DEST_DIR}"
 rsync $NFLAG -rvz --delete -e "ssh" --rsync-path="sudo rsync" "${DIR}/${DEPLOY_SOURCE_DIR}" "${DEPLOY_ACCOUNT}@${DEPLOY_SERVER}:${DEPLOY_DEST_DIR}"
